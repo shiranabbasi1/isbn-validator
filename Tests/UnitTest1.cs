4f66db5cf85fb5c9e3f89316b62b6be242323425
SHIRAN-ABBASI\In Stock <shiran@aymcommerce.com>
2022-02-26 20:03:33 +0500

    add unit tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,12 +1,36 @@
 using Xunit;
+using IsbnValidator.Interfaces;
+using IsbnValidator.Implementations;
+using IsbnValidator.Common.Enums;
 
-namespace tests;
+namespace Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("0-471-19047-0")]
+    [InlineData("0-471-19077-0")]
+    public void TenTest(string isbn)
     {
-
+        IIsbnValidator isbnValidator = IsbnValidatorResolver.Resolve(IsbnScheme.Ten);
+        bool isValid = isbnValidator.Validate(isbn);
+        if (isbn.Equals("0-471-19047-0"))
+            Assert.Equal(isValid, true);
+        else if (isbn.Equals("0-471-19077-0"))
+            Assert.Equal(isValid, false);
     }
+
+    [Theory]
+    [InlineData("9781861972712")]
+    [InlineData("9781681972712")]
+    public void ThirteenTest(string isbn)
+    {
+        IIsbnValidator isbnValidator = IsbnValidatorResolver.Resolve(IsbnScheme.Thirteen);
+        bool isValid = isbnValidator.Validate(isbn);
+        if (isbn.Equals("9781861972712"))
+            Assert.Equal(isValid, true);
+        else if (isbn.Equals("9781681972712"))
+            Assert.Equal(isValid, false);
+    }
+
 }
